81414e629b7b28d7bac9e97adfb21b78ee278af4
Paul Fonteneau <peyofonteneau8@gmail.com>
2024-01-21 02:16:57 +0100

    Mise en place d'Astro avec Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-/* eslint-disable turbo/no-underclared-env-vars */
+/* eslint-disable turbo/no-undeclared-env-vars */
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-import tailwind from "astrojs/tailwind";
+import tailwind from "@astrojs/tailwind";
 
 const SERVER_PORT = 3000;
-const LOCALHOST_URL = "http://localhost:" + SERVER_PORT;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 const LIVE_URL = "https://PAULFONTENEAU.github.io";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -20,7 +20,7 @@ export default defineConfig({
     integrations: [
         sitemap(),
         tailwind({
-            config: { applyBaseStyles: false },
-        }),
-    ],
+            config: { purge: false }, // Ajoutez cette ligne si nécessaire
+        })
+    ]
 });
