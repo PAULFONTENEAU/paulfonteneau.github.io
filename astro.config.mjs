7f705593c345409d413e5612e9dda80601ed2266
Paul Fonteneau <peyofonteneau8@gmail.com>
2024-01-21 01:40:58 +0100

    Update Tailwind CSS version and add .nojekyll file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwind from "astrojs/tailwind";
 
-
 const SERVER_PORT = 3000;
-const LOCALHOST_URL = "http://localhost:${SERVER_PORT}";
+const LOCALHOST_URL = "http://localhost:" + SERVER_PORT;
 const LIVE_URL = "https://PAULFONTENEAU.github.io";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -16,14 +15,12 @@ if (isBuild) {
 
 // https://astro.build/config
 export default defineConfig({
-    server: {port: SERVER_PORT },
-    site: BASE_URL
+    server: { port: SERVER_PORT },
+    site: BASE_URL,
     integrations: [
-        sitemap()
+        sitemap(),
         tailwind({
             config: { applyBaseStyles: false },
-        })
-    ]
-
+        }),
+    ],
 });
-
