dccefe309b40beb6aee17675a2ce203565cd4469
Boidin Benoît <benoit.bboidin@gmail.com>
2024-05-11 15:17:30 +0200

    Removing base parameter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,6 @@ if (isBuild) {
 export default defineConfig({
     server: { port: SERVER_PORT },
     site: BASE_URL,
-    base: 'paulfonteneau.github.io',
     integrations: [
         sitemap(),
         tailwind({
